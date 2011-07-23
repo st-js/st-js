c79827b8b7c45074467349099eba62572e315210
acraciun <ax.craciun@gmail.com>
2011-07-23 16:32:10 +0000

    moved stjs.js in the library

---
+++ js-lib/src/main/webapp/js/stjs.js
@@ -0,0 +1,57 @@
+/**** Functionality in JS, but not in Java ********
+ * functions (static methods of Global) to be called in Java
+ */
+
+
+/* String */
+/*
+anchor()
+big()
+blink()
+bold()
+fixed()
+fontcolor()
+fontsize()
+italics()
+link()
+small()
+strike()
+sub()
+sup()
+*/
+
+/* Number */
+function toFixed(n, pos){
+	return n.toFixed(pos);
+}
+/**** Functionality in Java, but not in JS ********
+ * methods added to JS prototypes
+ */
+
+/* String */
+/*
+codePointAt(int)
+codePointBefore(int)
+codePointCount(int, int)
+compareTo(String)
+compareToIgnoreCase(String)
+contentEquals(StringBuffer)
+endsWith(String)
+equalsIgnoreCase(String)
+getBytes()
+getBytes(int, int, byte[], int)
+getBytes(String)
+getBytes(Charset)
+getChars(char[], int)
+getChars(int, int, char[], int)
+matches(String)
+regionMatches(boolean, int, String, int, int)
+regionMatches(int, String, int, int)
+replaceFirst(String, String)
+replaceAll(String, String)
+startsWith(String)
+startsWith(String, int)
+trim()
+*/
+
+/* Number */
