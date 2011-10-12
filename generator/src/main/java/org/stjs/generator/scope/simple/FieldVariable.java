48cc90af4bceeae50a4e8221ccdca83b0e7831a0
eyalkaspi <ekaspi@gmail.com>
2011-10-12 04:22:08 +0000

    missing files

--- generator/src/main/java/org/stjs/generator/scope/simple/FieldVariable.java
+++ generator/src/main/java/org/stjs/generator/scope/simple/FieldVariable.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.stjs.generator.scope.simple;
 
 import java.lang.reflect.Field;
@@ -23,3 +24,33 @@ public class FieldVariable implements Variable {
 		return field;
 	}
 }
+=======
+package org.stjs.generator.scope.simple;
+
+import static org.stjs.generator.scope.classloader.ClassWrapper.wrap;
+
+import java.lang.reflect.Field;
+
+import org.stjs.generator.scope.classloader.ClassWrapper;
+
+public class FieldVariable implements Variable {
+
+	private final Field field;
+	
+	public FieldVariable(Field field) {
+		this.field = field;
+	}
+	
+	public ClassWrapper getType() {
+		return wrap(field.getType());
+	}
+	
+	public String getName() {
+		return field.getName();
+	}
+
+	public Field getField() {
+		return field;
+	}
+}
+>>>>>>> missing files
