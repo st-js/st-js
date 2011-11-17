2dd9a6e0037241420c2f5caea47d33417ba583b1
eyalkaspi <ekaspi@gmail.com>
2011-11-17 01:41:41 +0000

    declare package element type

--- shared/src/main/java/org/stjs/javascript/annotation/STJSBridge.java
+++ shared/src/main/java/org/stjs/javascript/annotation/STJSBridge.java
@@ -27,7 +27,7 @@ import java.lang.annotation.Target;
  * 
  * @author acraciun
  */
-@Target({ ElementType.TYPE })
+@Target({ ElementType.TYPE , ElementType.PACKAGE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface STJSBridge {//
 	String[] sources() default "";
