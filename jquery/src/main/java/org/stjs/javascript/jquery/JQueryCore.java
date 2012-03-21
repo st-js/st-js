543fe656fd7e7d6c1b0276aa7c97076ca629fab2
acraciun <ax.craciun@gmail.com>
2012-03-21 08:02:37 +0000

    added offset

--- jquery/src/main/java/org/stjs/javascript/jquery/JQueryCore.java
+++ jquery/src/main/java/org/stjs/javascript/jquery/JQueryCore.java
@@ -27,8 +27,8 @@ import org.stjs.javascript.functions.Function2;
 import org.stjs.javascript.utils.NameValue;
 
 /**
- * jquery interface for all jquery method + all used plugins in a web app. it can be mocked easily for testing purposes.
- * Compatible with jquery 1.6
+ * jquery interface for all jquery method + all used plugins in a web app. it can be mocked easily for testing purposes. Compatible with jquery
+ * 1.6
  */
 @SyntheticType
 public interface JQueryCore<FullJQuery extends JQueryCore<?>> {
@@ -50,8 +50,7 @@ public interface JQueryCore<FullJQuery extends JQueryCore<?>> {
 	/************* effects ***************/
 	public FullJQuery animate(Map<String, String> properties);
 
-	public FullJQuery animate(Map<String, String> properties, Object duration, String easing,
-			Callback1<Element> complete);
+	public FullJQuery animate(Map<String, String> properties, Object duration, String easing, Callback1<Element> complete);
 
 	public FullJQuery animate(Map<String, String> properties, AnimateOptions options);
 
@@ -207,6 +206,10 @@ public interface JQueryCore<FullJQuery extends JQueryCore<?>> {
 	public int outerWidth(boolean withMargin);
 
 	public Position offset();
+
+	public FullJQuery offset(Position placement);
+
+	public FullJQuery offset(Function2<Integer, Position, Position> offsetFunction);
 
 	public Position position();
 
