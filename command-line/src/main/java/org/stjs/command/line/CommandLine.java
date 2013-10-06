0454f6457424a333410bd5635892d552c7d49961
Alexandru Craciun <ax.craciun@gmail.com>
2013-10-06 18:04:15 +0200

    added command line support

---
+++ command-line/src/main/java/org/stjs/command/line/CommandLine.java
@@ -0,0 +1,82 @@
+package org.stjs.command.line;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
+
+import org.stjs.generator.GenerationDirectory;
+import org.stjs.generator.Generator;
+import org.stjs.generator.GeneratorConfiguration;
+import org.stjs.generator.GeneratorConfigurationBuilder;
+
+import com.google.common.base.Throwables;
+
+public class CommandLine {
+
+	public static void main(String[] args) throws URISyntaxException, IOException, ClassNotFoundException {
+		if (args.length != 2) {
+			System.err.println("Usage: CommandLine <srcDir> <classQualifiedName>");
+			return;
+		}
+		String path = args[0];
+		String fileName = args[1];
+		File sourceFile = new File(path + "/" + fileName.replaceAll("\\.", "/") + ".java");
+		compile(path, Collections.singletonList(sourceFile), Collections.<File>emptyList());
+		generate(path, fileName, Collections.<File>emptyList(), path);
+	}
+
+	static void generate(final String path, final String className, List<File> dependencies, String outputDir) {
+		try {
+			List<URL> classpathElements = new ArrayList<URL>();
+			classpathElements.add(new File(path).toURI().toURL());
+			for (File dep : dependencies) {
+				classpathElements.add(dep.toURI().toURL());
+			}
+
+			ClassLoader builtProjectClassLoader = new URLClassLoader(
+					classpathElements.toArray(new URL[classpathElements.size()]), Thread.currentThread()
+							.getContextClassLoader());
+			File sourceFolder = new File(path);
+			GenerationDirectory targetFolder = new GenerationDirectory(new File(outputDir), null, null);
+			File generationFolder = targetFolder.getAbsolutePath();
+
+			GeneratorConfigurationBuilder configBuilder = new GeneratorConfigurationBuilder();
+			configBuilder.allowedPackage(builtProjectClassLoader.loadClass(className).getPackage().getName());
+			GeneratorConfiguration configuration = configBuilder.build();
+			new Generator().generateJavascript(builtProjectClassLoader, className, sourceFolder, targetFolder,
+					generationFolder, configuration);
+		}
+		catch (Exception e) {
+			throw Throwables.propagate(e);
+		}
+	}
+
+	static void compile(final String path, final List<File> sourceFiles, List<File> dependencies) {
+		try {
+			JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+			StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+			fileManager.setLocation(StandardLocation.CLASS_PATH, dependencies);
+
+			Iterable<? extends JavaFileObject> compilationUnits1 = fileManager.getJavaFileObjectsFromFiles(sourceFiles);
+			compiler.getTask(null, fileManager, null, null, null, compilationUnits1).call();
+
+			fileManager.close();
+		}
+		catch (Exception e) {
+			throw Throwables.propagate(e);
+		}
+
+	}
+
+}
---
+++ command-line/src/main/java/org/stjs/command/line/ProjectCommandLine.java
@@ -0,0 +1,56 @@
+package org.stjs.command.line;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.io.File;
+import java.util.List;
+
+public class ProjectCommandLine {
+
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.err.println("Usate: CommandLine <srcDir> <libDir> <outputDir>");
+			return;
+		}
+		String path = args[0];
+		if (!path.endsWith(File.separator)) {
+			path += File.separator;
+		}
+		String libDir = args[1];
+		List<File> dependencies = listFiles(new File(libDir), ".jar");
+
+		String outputDir = args[2];
+		List<File> classNames = listFiles(new File(path), ".java");
+
+		CommandLine.compile(path, classNames, dependencies);
+		generate(path, classNames, dependencies, outputDir);
+
+	}
+
+	private static void generate(String path, List<File> files, List<File> dependencies, String outputDir) {
+		File srcPath = new File(path);
+		for (File file : files) {
+			// remove the leading srcPath from each file to get the source name
+			CommandLine.generate(path, file.getAbsolutePath().substring(srcPath.getAbsolutePath().length() + 1)
+					.replace(".java", "").replace(File.separatorChar, '.'), dependencies, outputDir);
+		}
+	}
+
+	private static List<File> listFiles(File srcDir, String suffix) {
+		List<File> files = newArrayList();
+		listFiles0(srcDir, files, suffix);
+		return files;
+	}
+
+	private static void listFiles0(File srcDir, List<File> output, String suffix) {
+		for (File file : srcDir.listFiles()) {
+			if (file.isDirectory()) {
+				listFiles0(file, output, suffix);
+			} else {
+				if (file.getName().endsWith(suffix)) {
+					output.add(file);
+				}
+			}
+		}
+	}
+}
