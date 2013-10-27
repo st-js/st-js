9ad6dfb2c494058d328b9fdbfbb106a27f39589f
npiguet <npiguet@romandie.com>
2013-10-27 22:36:29 +0100

    Added documentation to the SortFunction interface

--- shared/src/main/java/org/stjs/javascript/SortFunction.java
+++ shared/src/main/java/org/stjs/javascript/SortFunction.java
@@ -20,6 +20,43 @@ import org.stjs.javascript.annotation.Template;
 
 @JavascriptFunction
 public interface SortFunction<V> {
+	 
+	/**
+	 * Compares its two arguments for order. Returns a negative integer, zero, or a positive 
+	 * integer as the first argument is less than, equal to, or greater than the second.
+	 * 
+	 * <p>The notation:
+	 * <ul>
+	 * <li><em>a &lt;<sub>CF</sub> b</em> means <tt>comparefn(a,b) &lt; 0</tt>
+	 * <li><em>a =<sub>CF</sub> b</em> means <tt>comparefn(a,b) = 0</tt> (of either sign)
+	 * <li><em>a ><sub>CF</sub> b</em> means <tt>comparefn(a,b) > 0</tt>
+	 * </ul>
+	 * 
+	 * <p>A function <tt>comparefn</tt> is a consistent comparison function for a set of values 
+	 * <em>S</em> if all of the requirements below are met for all values <em>a</em>, 
+	 * <em>b</em>, and <em>c</em> (possibly the same value) in the set <em>S</em>: 
+	 * 
+	 * <ul>
+	 * <li>Calling <tt>comparefn(a,b)</tt> always returns the same value <tt>v</tt> when given a 
+	 *     specific pair of values <em>a</em> and <em>b</em> as its two arguments. Furthermore, 
+	 *     <tt>Type(v)</tt> is <tt>Number</tt>, and <tt>v</tt> is not <tt>NaN</tt>. Note that this 
+	 *     implies that exactly one of <em>a &lt;<sub>CF</sub> b</em>, <em>a =<sub>CF</sub> b</em>,
+	 *     and <em>a ><sub>CF</sub> b</em> will be true for a given pair of <em>a</em> and <em>b</em>.
+	 * <li>Calling <tt>comparefn(a,b)</tt> does not modify the this object 
+	 * <li><em>a =<sub>CF</sub> a</em> (reflexivity)
+	 * <li>If <em>a =<sub>CF</sub> b</em>, then <em>b =<sub>CF</sub> a</em> (symmetry)
+	 * <li>If <em>a =<sub>CF</sub> b</em> and <em>b =<sub>CF</sub> c</em>, then <em>a =<sub>CF</sub>
+	 *     c</em> (transitivity of =<sub>CF</sub>)
+	 * <li>If <em>a &lt;<sub>CF</sub> b</em> and <em>b &lt;<sub>CF</sub> c</em>, then <em>a 
+	 *     &lt;<sub>CF</sub> c</em> (transitivity of &lt;<sub>CF</sub>)
+	 * <li>If <em>a ><sub>CF</sub> b</em> and <em>b ><sub>CF</sub> c</em>, then <em>a ><sub>CF</sub>
+	 *     c</em> (transitivity of ><sub>CF</sub>)
+	 * </ul>
+	 * 
+	 * @param a the first object to be compared
+	 * @param b the second object to be compared
+	 * @return a negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.
+	 */
 	@Template("invoke")
 	public int $invoke(V a, V b);
 }
