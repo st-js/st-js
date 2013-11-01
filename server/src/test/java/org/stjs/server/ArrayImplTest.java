f3876f92243e08bb199ac004a2d7a3c62c64aa3c
npiguet <npiguet@romandie.com>
2013-11-01 22:51:48 +0100

    ArrayImplTest: added tests for shift, reverse and slice

--- server/src/test/java/org/stjs/server/ArrayImplTest.java
+++ server/src/test/java/org/stjs/server/ArrayImplTest.java
@@ -537,4 +537,363 @@ public class ArrayImplTest {
 		assertEquals(-1, x.$get(5));
 	}
 	
+	// ===========================================
+	// Tests section 15.4.4.8 of the ECMA-262 Spec
+	// ===========================================
+	// The elements of the array are rearranged so as to reverse their order.
+	// The object is returned as the result of the call
+	
+	@Test
+	public void testReverse01(){
+		Array<Integer> x = $array();
+		Array<Integer> reverse = x.reverse();
+		
+		assertSame(x, reverse);
+		assertEquals(0, x.$length());
+	}
+	
+	@Test
+	public void testReverse02(){
+		Array<Integer> x = $array();
+		x.$set(0, 1);
+		Array<Integer> reverse = x.reverse();
+		
+		assertSame(x, reverse);
+		assertEquals(1, x.$length());
+		assertEquals(1, x.$get(0).intValue());
+	}
+		
+	@Test
+	public void testReverse03(){
+		Array<Integer> x = $array(1,2);
+		Array<Integer> reverse = x.reverse();
+		
+		assertSame(x, reverse);
+		assertEquals(2, x.$get(0).intValue());
+		assertEquals(1, x.$get(1).intValue());
+		assertEquals(2, x.$length());
+	}
+	
+	@Test
+	public void testReverse04(){
+		Array<Object> x = $array();
+		x.$set(0, true);
+		x.$set(2, POSITIVE_INFINITY);
+		x.$set(4, null);
+		x.$set(5, null);
+		x.$set(8, "NaN");
+		x.$set(9, "-1");
+		Array<Object> reverse = x.reverse();
+		
+		assertSame(x, reverse);
+		assertEquals("-1", x.$get(0));
+		assertEquals("NaN", x.$get(1));
+		assertEquals(null, x.$get(2));
+		assertEquals(null, x.$get(3));
+		assertEquals(null, x.$get(4));
+		assertEquals(null, x.$get(5));
+		assertEquals(null, x.$get(6));
+		assertEquals(POSITIVE_INFINITY, x.$get(7));
+		assertEquals(null, x.$get(8));
+		assertEquals(true, x.$get(9));
+
+		x.$length(9);
+		reverse = x.reverse();
+
+		assertSame(x, reverse);
+		assertEquals(null, x.$get(0));
+		assertEquals(POSITIVE_INFINITY, x.$get(1));
+		assertEquals(null, x.$get(2));
+		assertEquals(null, x.$get(3));
+		assertEquals(null, x.$get(4));
+		assertEquals(null, x.$get(5));
+		assertEquals(null, x.$get(6));
+		assertEquals("NaN", x.$get(7));
+		assertEquals("-1", x.$get(8));
+	}
+	
+	// ===========================================
+	// Tests section 15.4.4.9 of the ECMA-262 Spec
+	// ===========================================
+	//  The first element of the array is removed from the array and
+	// returned
+	// If length equal zero, call the [[Put]] method of this object
+	// with arguments "length" and 0 and return undefined
+	
+	@Test
+	public void testShift01(){
+		Array<Object> x = $array();
+		Object shift = x.shift();
+		
+		assertNull(shift);
+		assertEquals(0, x.$length());
+	}
+	
+	@Test
+	public void testShift02(){
+		Array<Integer> x = $array(1,2,3);
+		x.$length(0);
+		Integer shift = x.shift();
+		
+		assertNull(shift);
+		assertEquals(0, x.$length());  
+	}
+	
+	@Test
+	public void testShift03(){
+		Array<Integer> x = $array(0,1,2,3);
+		Integer shift = x.shift();
+		
+		assertEquals(0, shift.intValue());
+		assertEquals(3, x.$length());
+		assertEquals(1, x.$get(0).intValue());
+		assertEquals(2, x.$get(1).intValue());
+	}
+	
+	@Test
+	public void testShift04(){
+		Array<Integer> x = $array();
+		x.$set(0, 0);
+		x.$set(3, 3);
+		Integer shift = x.shift();
+		
+		assertEquals(0, shift.intValue());
+		assertEquals(3, x.$length());
+		assertEquals(null, x.$get(0));
+		assertEquals(null, x.$get(12));
+		
+		x.$length(1);
+		shift = x.shift();
+		
+		assertNull(shift);
+		assertEquals(0, x.$length());
+	}
+	
+	// ============================================
+	// Tests section 15.4.4.10 of the ECMA-262 Spec
+	// ============================================
+	// 
+	// If start is positive, use min(start, length).
+	// If start is negative, use max(start + length, 0).
+	// If end is positive, use min(end, length)
+	// If end is negative, use max(end + length, 0)
+	//
+	// Note: we are not including tests that check for the correct behavior to slice() 
+	// when the end or start parameters are not numbers, because our interface declaration
+	// does not allow that (and it seems like a bad practice anyway)
+	
+	@Test
+	public void testSlice01(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(0,3);
+
+		assertEquals(3, arr.$length());
+		assertEquals(0, arr.$get(0).intValue());
+		assertEquals(1, arr.$get(1).intValue());
+		assertEquals(2, arr.$get(2).intValue());
+		assertEquals(null, arr.$get(3));
+	}
+	
+	@Test
+	public void testSlice02(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(3,3);
+
+		assertEquals(0, arr.$length());
+		assertEquals(null, arr.$get(0));
+	}
+	
+	@Test
+	public void testslice03(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(4,3);
+
+		assertEquals(0, arr.$length());
+		assertEquals(null, arr.$get(0));
+	}
+	
+	@Test
+	public void testslice04(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(5,5);
+
+		assertEquals(0, arr.$length());
+		assertEquals(null, arr.$get(0));
+	}
+
+	@Test
+	public void testSlice05(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(3,5);
+
+		assertEquals(2, arr.$length());
+		assertEquals(3, arr.$get(0).intValue());
+		assertEquals(4, arr.$get(1).intValue());
+		assertEquals(null, arr.$get(3));
+	}
+
+	@Test
+	public void testSlice06(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(2,4);
+
+		assertEquals(2, arr.$length());
+		assertEquals(2, arr.$get(0).intValue());
+		assertEquals(3, arr.$get(1).intValue());
+		assertEquals(null, arr.$get(3));
+	}
+	
+	@Test
+	public void testSlice07(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(3,6);
+
+		assertEquals(2, arr.$length());
+		assertEquals(3, arr.$get(0).intValue());
+		assertEquals(4, arr.$get(1).intValue());
+		assertEquals(null, arr.$get(3));
+	}
+	
+	@Test
+	public void testSlice08(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-3,3);
+
+		assertEquals(1, arr.$length());
+		assertEquals(2, arr.$get(0).intValue());
+		assertEquals(null, arr.$get(1));
+	}
+	
+	@Test
+	public void testSlice09(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-1,5);
+
+		assertEquals(1, arr.$length());
+		assertEquals(4, arr.$get(0).intValue());
+		assertEquals(null, arr.$get(1));
+	}
+	
+	@Test
+	public void testSlice10(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-5,1);
+
+		assertEquals(1, arr.$length());
+		assertEquals(0, arr.$get(0).intValue());
+		assertEquals(null, arr.$get(1));
+	}
+	
+	@Test
+	public void testSlice11(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-9,5);
+
+		assertEquals(5, arr.$length());
+		assertEquals(0, arr.$get(0).intValue());
+		assertEquals(0, arr.$get(1).intValue());
+		assertEquals(0, arr.$get(2).intValue());
+		assertEquals(0, arr.$get(3).intValue());
+		assertEquals(0, arr.$get(4).intValue());
+		assertEquals(null, arr.$get(5));
+	}
+	
+	@Test
+	public void testSlice12(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(0,-2);
+
+		assertEquals(3, arr.$length());
+		assertEquals(0, arr.$get(0).intValue());
+		assertEquals(0, arr.$get(1).intValue());
+		assertEquals(0, arr.$get(2).intValue());
+		assertEquals(null, arr.$get(3));
+	}
+	
+	@Test
+	public void testSlice13(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(1,-4);
+
+		assertEquals(0, arr.$length());
+		assertEquals(null, arr.$get(0));
+	}
+	
+	@Test
+	public void testSlice14(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(0,-5);
+
+		assertEquals(0, arr.$length());
+		assertEquals(null, arr.$get(0));
+	}
+	
+	@Test
+	public void testSlice15(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(4,-9);
+
+		assertEquals(0, arr.$length());
+		assertEquals(null, arr.$get(0));
+	}
+	
+	@Test
+	public void testSlice16(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-5,-2);
+
+		assertEquals(3, arr.$length());
+		assertEquals(0, arr.$get(0).intValue());
+		assertEquals(0, arr.$get(1).intValue());
+		assertEquals(0, arr.$get(2).intValue());
+		assertEquals(null, arr.$get(3));
+	}
+	
+	@Test
+	public void testSlice17(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-3,-1);
+
+		assertEquals(2, arr.$length());
+		assertEquals(2, arr.$get(0).intValue());
+		assertEquals(3, arr.$get(1).intValue());
+		assertEquals(null, arr.$get(2));
+	}
+	
+	@Test
+	public void testSlice18(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-9,-1);
+
+		assertEquals(4, arr.$length());
+		assertEquals(0, arr.$get(0).intValue());
+		assertEquals(1, arr.$get(1).intValue());
+		assertEquals(2, arr.$get(2).intValue());
+		assertEquals(3, arr.$get(3).intValue());
+		assertEquals(null, arr.$get(4));
+	}
+	
+	@Test
+	public void testSlice19(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-6,-6);
+
+		assertEquals(0, arr.$length());
+		assertEquals(null, arr.$get(0));
+	}
+	
+	@Test
+	public void testSlice20(){
+		Array<Integer> x = $array(0,1,2,3,4);
+		Array<Integer> arr = x.slice(-2);
+
+		assertEquals(2, arr.$length());
+		assertEquals(3, arr.$get(0).intValue());
+		assertEquals(4, arr.$get(1).intValue());
+		assertEquals(null, arr.$get(2)); 
+	}
+	
+	// ============================================
+	// Tests section 15.4.4.11 of the ECMA-262 Spec
+	// ============================================
 }
