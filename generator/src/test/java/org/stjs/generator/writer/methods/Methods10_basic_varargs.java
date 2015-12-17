92efd79b9819f356402a094ac83a65a7ccdd5b7a
Mathieu Cote <mcote@mirego.com>
2015-12-17 15:52:43 -0500

    Fix array varargs generation
    An issue with the special Array class implemented by StJs was causing special overboard effects.

--- generator/src/test/java/org/stjs/generator/writer/methods/Methods10_basic_varargs.java
+++ generator/src/test/java/org/stjs/generator/writer/methods/Methods10_basic_varargs.java
@@ -1,16 +1,21 @@
 package org.stjs.generator.writer.methods;
 
-public class Methods10_basic_varargs {
+public class Methods10_basic_varargs<T> {
 	public static String main(String[] args) {
-		String test = method(0);
-		return method(0, "1", "2", "3");
+		String test = new Methods10_basic_varargs<String>().method(0);
+		return new Methods10_basic_varargs<String>().method(0, "1", "2", "3");
 	}
 
-	public static String method(int param, String... strings) {
+	public String method(int param, T... strings) {
 		String joinedStrings = "";
-		for (String string : strings) {
+		for (T string : strings) {
 			joinedStrings += string;
 		}
+		new Methods10_basic_varargs<String>().method2("1");
 		return joinedStrings;
+	}
+
+	public String method2(T... strings) {
+		return strings.toString();
 	}
 }
