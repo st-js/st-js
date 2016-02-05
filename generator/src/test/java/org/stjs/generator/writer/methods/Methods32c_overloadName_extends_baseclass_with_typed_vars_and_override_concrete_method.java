ce9ad721db53b05d190ac7d66254e688606396c5
Mathieu Baron <mbaron@mirego.com>
2016-02-04 23:42:58 -0500

    Fix tests

--- generator/src/test/java/org/stjs/generator/writer/methods/Methods32c_overloadName_extends_baseclass_with_typed_vars_and_override_concrete_method.java
+++ generator/src/test/java/org/stjs/generator/writer/methods/Methods32c_overloadName_extends_baseclass_with_typed_vars_and_override_concrete_method.java
@@ -2,14 +2,15 @@ package org.stjs.generator.writer.methods;
 
 public class Methods32c_overloadName_extends_baseclass_with_typed_vars_and_override_concrete_method {
 
+    public abstract class AbstractClass<T> {
+        protected void aMethod(T item) {
+        }
+    }
+
     public class SpecificType<T extends String> extends AbstractClass<T> {
         @Override
         protected void aMethod(T item) {
         }
     }
 
-    public abstract class AbstractClass<T> {
-        protected void aMethod(T item) {
-        }
-    }
 }
