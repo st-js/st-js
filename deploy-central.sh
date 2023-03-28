fac515054dc00c1947d1d3833b188822bdd55739
Thanh Do <thanh.doson@swissquote.ch>
2023-03-28 10:58:49 +0200

    Update to new version

--- deploy-central.sh
+++ deploy-central.sh
@@ -43,3 +43,25 @@ mvn gpg:sign-and-deploy-file \
 -DpomFile=maven-plugin/pom.xml \
 -Dfile=maven-plugin/target/stjs-maven-plugin-$VERSION-sources.jar \
 -Dclassifier=sources
+
+
+# test-helper
+mvn gpg:sign-and-deploy-file \
+-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+-DrepositoryId=ossrh \
+-DpomFile=generator-plugin-java8/pom.xml \
+-Dfile=generator-plugin-java8/target/stjs-generator-plugin-java8-$VERSION.jar
+
+mvn gpg:sign-and-deploy-file \
+-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+-DrepositoryId=ossrh \
+-DpomFile=generator-plugin-java8/pom.xml \
+-Dfile=generator-plugin-java8/target/stjs-generator-plugin-java8-$VERSION-javadoc.jar \
+-Dclassifier=javadoc
+
+mvn gpg:sign-and-deploy-file \
+-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+-DrepositoryId=ossrh \
+-DpomFile=generator-plugin-java8/pom.xml \
+-Dfile=generator-plugin-java8/target/stjs-generator-plugin-java8-$VERSION-sources.jar \
+-Dclassifier=sources
